6385972069f47e32c10710ca284a5f4b922bbfc8
phamlong1912 <rsphamlong1912@gmail.com>
2023-02-09 20:58:58 +0700

    Sign in with Google

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -19,7 +19,7 @@ const Account = () => {
 
   return <div className="max-w-[600px] mx-auto my-16 p-4">
     <h1 className="text-2xl font-bold py-4">Account</h1>
-    <p>User Email: {user && user.email}</p>
+    <p>User Email: {user && user.displayName}</p>
     <button onClick={handleLogout} className="border px-6 py-2 my-4">Logout</button>
   </div>;
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+//Initialize Firebase Authentication with Google
+export const provider = new GoogleAuthProvider();
+
 export default app;
